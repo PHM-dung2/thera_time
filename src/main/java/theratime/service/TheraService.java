6dc41985c6dc5a6d73017e5a3584bca29e4200a7
PHM-dung2 <pp4992@naver.com>
2025-06-27 01:41:36 +0900

    6/27 치료시간 관리 개인 프로젝트 시간표 조회

--- src/main/java/theratime/service/TheraService.java
+++ src/main/java/theratime/service/TheraService.java
@@ -6,6 +6,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
+import theratime.model.dto.TheraDto;
 import theratime.model.repository.TheraRepository;
 import theratime.util.ApiResponse;
 
@@ -16,7 +17,6 @@ public class TheraService {
     private final TheraRepository theraRepository;
 
     // 1. 시간표 등록
-    @PostMapping("/write")
     public ApiResponse<Boolean> onWrite(String token ){
         if( token == null ) return new ApiResponse<>( false, "시간표 등록 실패", null );
 
@@ -25,6 +25,11 @@ public class TheraService {
     }
 
     // 2. 시간표 전체 조회
+    public ApiResponse<TheraDto> findAll(String token){
+
+        TheraDto theraDto = new TheraDto();
+        return new ApiResponse<>( true, "시간표 조회 성공", theraDto );
+    }
 
     // 3. 시간표 개별 조회
 
